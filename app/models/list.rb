b8400a7b28cd8be3929623a96387ce4840223934
EC2 Default User <ec2-user@ip-172-31-34-3.ap-northeast-1.compute.internal>
2024-05-30 14:42:57 +0000

    Initial Commit3

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,2 +1,5 @@
 class List < ApplicationRecord
+  # Listモデルにtitleとbodyに加えて
+  # 画像を扱うためのimageカラムを追記
+  has_one_attached :image
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
-  get 'lists/new'
   get 'top' => 'homes#top'
-  post 'lists' => 'lists#create'
-  get 'lists' => 'lists#index'
-  # .../lists/1や.../lists/3に該当
-  # どの投稿データを表示するか判別するために
-  # 投稿データのidも含める  
-  # asオプションでルーティングに名前を付ける
-  get 'lists/:id' => 'lists#show', as: 'list'
-
-  get 'lists/edit'
+  resources :lists
+  # get 'lists/new'
+  # post 'lists' => 'lists#create'
+  # get 'lists' => 'lists#index'
+  # # .../lists/1や.../lists/3に該当
+  # # どの投稿データを表示するか判別するために
+  # # 投稿データのidも含める  
+  # # asオプションでルーティングに名前を付ける
+  # get 'lists/:id' => 'lists#show', as: 'list'
+  # get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
+  # patch 'lists/:id' => 'lists#update', as: 'update_list'
+  # delete 'lists/:id' => 'lists#destroy', as: 'destroy_list'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
